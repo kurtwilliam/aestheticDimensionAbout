d20551352e37735bff45af0feed1c6f22dc26bca
Kurt Schneider <k.w.schneid@gmail.com>
2017-11-03 21:44:59 -0400

    Restarting?

---
+++ src/scripts/app.js
@@ -0,0 +1,253 @@
+ // Ch
+
+// Home SVG
+
+// Get SVG's and LI
+
+let home = document.getElementById('home');
+let homeHover = document.getElementById('homeHover');
+let homeSVG = document.getElementById('homeSVG');
+
+// Attach Event Listener 
+homeSVG.addEventListener('mouseover', homeOver);
+home.addEventListener('mouseover', homeOver);
+homeSVG.addEventListener('mouseout', homeOut);
+
+// run hover function and hoverout function
+function homeOver(x) {
+	// home.classList.add('vHiddenHome')
+	homeHover.classList.remove('vHiddenHome')
+}
+
+function homeOut(x) {
+	// home.classList.remove('vHiddenHome')
+	homeHover.classList.add('vHiddenHome')
+}
+
+
+// Get form, if it exists run code
+let form = document.getElementById('form');
+if ( form ) {
+	let meterInput = document.getElementsByClassName('meterInput');
+
+	Array.from(meterInput).forEach(function(e) {
+     e.addEventListener('input', meterWidth);
+   });
+
+	function meterWidth(x) {
+		let input = this.value;
+		let meter = this.nextElementSibling;
+		meter.style.width = '100%';
+
+		if (input.match(/\D/) == null) {
+			meter.style.width = '0%';
+		}
+
+		let firstName = document.getElementById('firstName').value;
+		let lastName = document.getElementById('lastName').value;
+		let email = document.getElementById('email').value;
+		let comments = document.getElementById('comments').value;
+		let bCL = document.getElementById('buttonCoverL')
+		let bCR = document.getElementById('buttonCoverR')
+		let cB = document.getElementById('contactButton')
+
+		if (firstName.match(/\D/) !== null && lastName.match(/\D/) !== null && email.match(/\D/) !== null && comments.match(/\D/) !== null ) {
+			bCL.style.width = '0'
+			bCL.style.left = '100px'
+			bCR.style.width = '0'
+
+			// function pulseGreenActive() {
+			// 	bCL.style.zIndex = '-10'
+			// 	bCR.style.zIndex = '-10'
+
+
+			// 	// Add class to make 
+			// 	cB.classList.add
+			// }
+		} else {
+			bCL.style.width = '100px'
+			bCL.style.left = '0px'
+			bCR.style.width = '100.5px'
+		}
+	} 
+};
+
+
+burger.addEventListener('click', (e) => {
+	console.log('hi')
+	burger.classList.toggle('open')
+});
+
+// never even runs...
+function windowResize() {
+	if (window.matchMedia("(min-width: 769px)").matches) {	
+	
+	} else {
+		burger.classList.remove("vHidden");
+		burger.addEventListener('click', (e) => {
+			// if burger has class of open move menu up
+			if (burger.classList.contains('open')) {
+				leftColumn.style.top = '0'
+				// burger.classList.remove('open')
+			// else move it back
+			} else {
+				leftColumn.style.top = '-323px'
+				// burger.classList.add('open')
+			}
+		});
+	}
+}
+
+var w = window.innerWidth;
+
+function resize() { 
+	w = window.innerWidth;
+
+	let leftColumn = document.getElementById('leftColumn');
+
+	if ( w < 768 ) {
+		burger.classList.remove("vHidden");
+		leftColumn.classList.remove('under300L')
+
+		burger.addEventListener('click', (e) => {
+			// if burger has class of open move menu up
+			if (burger.classList.contains('open')) {
+				leftColumn.style.top = '0'
+				leftColumn.style.left = '0'
+				// burger.classList.remove('open')
+			// else move it back
+			} else {
+				leftColumn.style.top = '-323px'
+				leftColumn.style.left = '0'
+				// burger.classList.add('open')
+			}
+		});
+		burger.classList.add('mobileBurger');
+		leftColumn.style.top = '-323px'
+
+	} else {
+		burger.classList.remove('mobileBurger');
+		burger.classList.add("vHidden");
+		leftColumn.style.top = '0'
+		burger.addEventListener('click', (e) => {
+			// if burger has class of open move menu over
+			if (burger.classList.contains('open')) {
+				leftColumn.style.left = '0'
+				leftColumn.style.top = '0'
+
+				// burger.classList.remove('open')
+			// else move it back
+			} else {
+				leftColumn.style.left = '-290px'
+				leftColumn.style.top = '0'
+
+				// burger.classList.add('open')
+			}
+		});
+	}
+}
+
+resize();
+
+window.onresize = resize;
+
+// Scroll function for sidebar shrinking under 300px
+
+window.onscroll=testScroll;
+
+function testScroll(){
+	let burger = document.getElementById('burger');
+	let leftColumn = document.getElementById('leftColumn');
+	let rightColumn = document.getElementById('rightColumn');
+	let burgerMenu = document.getElementById('burgerMenu');
+	let responsiveTitle = document.getElementById('responsiveTitle');
+	let socialMedia = document.getElementById('socialMedia');
+
+
+	// If screen is wider than 768px run if statement below
+	if (window.matchMedia("(min-width: 769px)").matches) {
+
+		// burger on click move left column over to see menu options
+		burger.addEventListener('click', (e) => {
+			// if burger has class of open move menu over
+			if (burger.classList.contains('open')) {
+				leftColumn.style.left = '0'
+				leftColumn.style.top = '0'
+
+				// burger.classList.remove('open')
+			// else move it back
+			} else {
+				leftColumn.style.left = '-290px'
+				leftColumn.style.top = '0'
+
+				// burger.classList.add('open')
+			}
+		});	
+		// if the scroll is lower than 250px from top
+
+		if(window.pageYOffset>250 && socialMedia) {
+			// if the scroll is lower than 250px from top move left sidebar over and make skinny, and make right sidebar wider, and add burger menu
+			leftColumn.classList.add('under300L');
+			rightColumn.classList.add('under300R');
+			burger.classList.add('under300Burger');
+			burger.classList.remove('vHidden');
+			leftColumn.style.left = '-290px'
+
+		} else {
+			// if the scroll isn't lower than 250px from top move back to normal setup and make right sidebar normal 
+			leftColumn.classList.remove('under300L');
+			rightColumn.classList.remove('under300R');
+			burger.classList.add('vHidden');
+			burger.classList.remove('under300Burger');
+			
+			leftColumn.style.left = '0'
+			// responsiveTitle.classList.add('hidden')
+		}
+	// else if screen is less than 769px
+	} else {
+
+	}
+}
+
+// search input show/hide
+
+// check if on blog page
+
+let blogSearch = document.getElementById('blogSearch')
+
+if (blogSearch) {
+	// get elements 
+
+	let blogSearchInput = document.getElementById('blogSearchInput')
+	let blogSearchContainer = document.getElementById('blogSearchContainer')
+	let blogSearchMeter = document.getElementById('blogSearchMeter')
+	let blogSearchBar = document.getElementById('blogSearchBar')
+	let blogSearchImg = document.getElementById('blogSearchImg')
+
+	blogSearchImg.addEventListener('click', blogSearchWidth);
+	
+	function blogSearchWidth() {
+		blogSearch.style.width = "calc(100% - 38px)";
+		blogSearchContainer.style.width = "300px";
+		blogSearchInput.style.width = "100%";
+		blogSearchInput.style.padding = "1px 1px 1px 0";
+		blogSearchMeter.style.width = "calc(100% - 10px)";
+		blogSearchBar.style.width = "calc(100% - 10px)";
+
+		if (blogSearchContainer.offsetWidth >= 80) {
+			blogSearch.style.width = "0";
+			blogSearchContainer.style.width = "38px";
+			blogSearchInput.style.padding = "0";
+			blogSearchInput.style.width = "0";
+			blogSearchMeter.style.width = "0";
+			blogSearchBar.style.width = "0";
+		}
+	}
+}
+
+let mailLink = document.querySelector('.mailLink');
+
+if (mailLink) {
+	var index = mailLink.stringify.indexOf('@')
+	console.log(index);
+}
